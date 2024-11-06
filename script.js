e79e78d17feddeb555f7ba4791be63faf88da6af
Bruna Antunes <brunaantunesrds@gmail.com>
2024-11-06 20:37:02 -0300

    Update script.js
     inserção js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(Frase) {
+
+
+    const texto = Frase.replace(/\s/g, '').toLowerCase()
+    const tamanho = texto.length
+  
+    for (let i = 0; i < tamanho; i++) {
+        if (texto[i] !== texto[tamanho - 1 - i]) {
+            return false 
+        }
+    }
+  
+    return true 
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+
+    function arrayMaxMin(arr) {
+
+        let min = arr[0];
+        let max = arr[0];
+    
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i]; 
+            }
+            if (arr[i] > max) {
+                max = arr[i]; 
+            }
+        }
+    
+        return [min, max];
+    }
+    
+
+    
+
